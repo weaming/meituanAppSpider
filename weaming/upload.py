c135af9278dfa028c66dbd5a32893f92b539eefa
weaming <garden.yuen@gmail.com>
2019-10-24 13:15:23 +0800

    update upload.py

--- weaming/upload.py
+++ weaming/upload.py
@@ -2,14 +2,12 @@
 # Author    : weaming
 # Mail      : garden.yuen@gmail.com
 # Created   : 2019-10-24 12:58:27
-import json
 import requests
 from data_process.io_csv import read_csv
 
 data = read_csv('./SZpure.csv')
 print(data)
 for x in data:
-    res = requests.get(
-        f'https://kv.drink.cafe/sadd/lunch/{json.dumps(x, ensure_ascii=False)}'
-    )
+    value = f"{x['avgScore']} | {x['title']} | {x['address']} | ￥{x['avgPrice']}"
+    res = requests.get(f'https://kv.drink.cafe/sadd/lunch/{value}')
     print(res.json())
